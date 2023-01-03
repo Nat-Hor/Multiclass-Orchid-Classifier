aeccb5f77b15257f06323e991139ce81af950e70
Nat-Hor <121566568+Nat-Hor@users.noreply.github.com>
2023-01-03 12:07:40 +0800

    Add files via upload

---
+++ orchid_info.py
@@ -0,0 +1,5 @@
+orchid_namelist = ["Dendrobium dawn maree", "Renanthera Kalsom", "Vanda Miss Joaquim", "Aerides houlletiana",
+                "Brassavola nodosa", "Bulbophyllum annandalei", "Bulbophyllum lepibum", "Calanthe sylvatica",
+                "Coelogyne pandurata", "Cymbidium bicolor", "Eria floribunda", "Grammtophyllum speciosum",
+                "Paphiopedilum callosum", "Phalaenopsis lowii", "Phaleanopsis violacea", "Spathoglottis plicata"]
+
