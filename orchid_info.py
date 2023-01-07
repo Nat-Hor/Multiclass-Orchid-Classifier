9f712ee77315d362f7c6c089777c0566052ee3a8
Nat-Hor <121566568+Nat-Hor@users.noreply.github.com>
2023-01-07 20:34:44 +0800

    Delete orchid_info.py

--- orchid_info.py
+++
@@ -1,5 +0,0 @@
-orchid_namelist = ["Dendrobium dawn maree", "Renanthera Kalsom", "Vanda Miss Joaquim", "Aerides houlletiana",
-                "Brassavola nodosa", "Bulbophyllum annandalei", "Bulbophyllum lepibum", "Calanthe sylvatica",
-                "Coelogyne pandurata", "Cymbidium bicolor", "Eria floribunda", "Grammtophyllum speciosum",
-                "Paphiopedilum callosum", "Phalaenopsis lowii", "Phaleanopsis violacea", "Spathoglottis plicata"]
-
