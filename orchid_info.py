9e3910a36d7ceb0786472aabd0a30fc073101890
Nat-Hor <121566568+Nat-Hor@users.noreply.github.com>
2023-01-07 20:35:12 +0800

    Add files via upload

---
+++ orchid_info.py
@@ -0,0 +1,72 @@
+orchid_namelist = ["Dendrobium dawn maree", "Renanthera Kalsom", "Papilionanthe Miss Joaquim", "Aerides houlletiana",
+                "Brassavola nodosa", "Bulbophyllum annandalei", "Bulbophyllum lepidum", "Calanthe sylvatica",
+                "Coelogyne pandurata", "Cymbidium bicolor", "Eria floribunda", "Grammtophyllum speciosum",
+                "Paphiopedilum callosum", "Phalaenopsis lowii", "Phaleanopsis violacea", "Spathoglottis plicata"]
+
+orchid_description = [
+    #1
+    """This species is a cross between Dendrobium formosum and Dendrobium cruentum.
+    \nIt grows with moderate shade, frequent watering, and intermediate temperature.
+    \nIt is a frequent bloomer, and its flower is long-lasting, with massive fragrance at night.""",
+    #2
+    """This species is a cross berween Renanthera philippinensis and Renanthera storiei.
+    \nIt grows with full to partial sun, high humidity, little fertilization, frequent watering, and warm conditions.
+    \nIt is a free flowering plant.""",
+    #3
+    """This species is a cross between Papilionanthe teres and Papilionanthe hookeriana.
+    \nIt is a robust sun loving plant that requires heavy fertilizing, vertical support to enable it to grow straight and tall along with free air movement and high humidity.
+    \nIt is a free flowering plant.""",
+    #4
+    """This species is epiphytic, it is found in evergreen, semi-deciduous and deciduous dry lowland forests at elevations of sealevel to 700 meters.
+    \nIt grows with partial shade and hot to warm conditions.
+    \nIts flowers are quite fragrant, and bloom in the spring and summer.""",
+    #5
+    """This species is epiphytic or lithophytic, it is found from sea level to about 500 meters.
+    \nIt grows with moderate shade and high humidity, it is quite salt tolerant.
+    \nIts flower has a nice, citrus fragrance at night, blooms in the spring, summer and fall.""",
+    #6
+    """This species is epiphytic, it is found in penninsular Thailand and Malaysia at elevations around 1000 meters.
+    \nIt grows with partial shade and cool to warm conditions.
+    \nIts flower blooms in the spring.""",
+    #7
+    """This species is epiphytic, it is found in semi-deciduous and evergreen forests on mossy rocks and tree bases at elevations of 300 to 1100 meters.
+    \nIt requires regular waterings and fertilizer, high humidity and good air circullation.
+    \nIts flower blooms in the summer and fall.""",
+    #8
+    """This species is terrestrial, it is found in broadleafed, evergreen, humid primary forests in soils and splash along steep watercourses in humus and deep shade at elevations of 400 to 2700 meters.
+    \nIt grows with partial shade and cool to hot conditions.
+    \nIts flower blooms in the spring and summer.""",
+    #9
+    """This species is epiphytic, it is found on large trees near rivers or terrestrial.
+    \nIt needs wire basket culture as it spreads out quite rapidly and sphagnum with wood chips as media works best.
+    \nIts flower blooms in late spring-summer out of the center of newly emerging growths, and is highly fragrant of honey.""",
+    #10
+    """This species is epiphytic, it is found in evergreen broadleaf, dry lowland forests and savana-like woodlands at elevations of 400 to 1100 meters.
+    \nIt prefers even year round, humid and warm conditions with regular fertilizer.
+    \nIts flower blooms in the spring and summer.""",
+    #11
+    """This species is epiphytic, it is found in primary montane forests on large trees along streams at elevations of 500 to 2400 meters.
+    \nPlant it in a pot with medium fine bark and treefern mix and give bright shade with a regular weekly fertilizing while growing and a lessening when not.
+    \nIts flower blooms in the spring.""",
+    #12
+    """This species is epiphytic and occasional lithophytic, it is found at lowland forests near streams and rivers at elevations of 100 to 1200 meters.
+    \nIt is recorded to be the heaviest orchid in the world and is capable of becoming huge in cultivation.
+    \nIts flowers are long-lasting, but it can be slow to bloom, plants need to be large and have a lot of backbulbs.""",
+    #13
+    """This species is terrestrial or lithophytic, it is found in primary, broadleafed lowland forests, coniferous woods, primary highland cloud forests at altitudes of 300 to 2000 meters.
+    \nIt grows with even water and fertilizer, plus humid, very shady conditions, year round.
+    \nIts flower blooms in the spring and early summer.""",
+    #14
+    """This species is epiphytic or sometimes lithophytic, it is found in conjunction with limestone rocks at elevations around sealevel to 500 meters near rivers.
+    \nIt is best to grow it wetter because it experiences a long drought in nature, and keep 3 to 5 leaves for optimum growing.
+    \nIts flower blooms in the summer and early fall.""",
+    #15
+    """This species is epiphytic, it is found in the Malaya peninsula and the Indonesian island of Sumatra at elevations of sea level to 150 meters.
+    \nIt requires deep shade and high humidity as well as being watered regularly and should never dry out completely.
+    \nIt has extremely fragrant flowers occuring in the spring.""",
+    #16
+    """This species is terrestrial, it is found at rocky grassland slopes, in dry lowland forests and savana-like woodlands at elevations of 700 to 2000 meters.
+    \nIt can take full sun year round and require regular waterings and fertilizer year round.
+    \nIts nonfragrant flowers that can bloom several times in a year."""
+    ]
+
